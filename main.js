19979349cdfb7f5581d525c768e4cb7482fd6185
Abduldb09 <86055842+AbdulDeBorja09@users.noreply.github.com>
2023-11-01 18:14:36 +0800

    Login

--- main.js
+++ main.js
@@ -12,3 +12,46 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function validateForm() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (
+    email === "dimagibat@students.nu-laguna.edu.ph" &&
+    password === "Dimagibs1234"
+  ) {
+    alert("Login successful!");
+    window.location.href = "index.html";
+    return false;
+  } else if (
+    email === "juandc@librarian.nu-laguna.edu.ph" &&
+    password === "  "
+  ) {
+    alert("Login successful!");
+    window.location.href = "Librarian_home.html";
+    return false;
+  } else if (
+    email === "dalisayc@admin.nu-laguna.edu.ph" &&
+    password === "CardoD123"
+  ) {
+    alert("Login successful!");
+    window.location.href = "Admin_home.html";
+    return false;
+  } else {
+    alert("Incorrect login details");
+    return false;
+  }
+}
+
+const container = document.getElementById("singin-container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
